aa528fff938a8a42f7c6a54865105a4f1baef700
Pete Hodgson <git@thepete.net>
2015-12-29 14:32:39 -0800

    extract generic marble rendering code into lib/marbles.js

---
+++ js/lib/marbles.js
@@ -0,0 +1,39 @@
+import Rx from 'rx';
+import Immutable from 'immutable';
+import createInitialRealizer from './virtualDomRealizer';
+import renderMarbles from './visual';
+
+export default function createMarbleDisplay(containerElement){
+
+  const initialRealizer = createInitialRealizer(containerElement);
+
+  let observations = new Rx.Subject(); 
+
+  function recordObservation(streamId,observation){
+    observations.onNext({streamId,observation});
+  }
+
+
+  const obsState = observations.scan( function(observables,{streamId,observation}){
+    const EMPTY_OBSERVABLE = Immutable.Map({streamName:streamId,observations:Immutable.List()})
+
+    return observables.update( streamId, EMPTY_OBSERVABLE, function(observable){
+      return observable.update('observations', (obs)=> obs.push(observation));
+    });
+  },Immutable.Map());
+
+  const animationFrames = Rx.Observable.interval(50,Rx.Scheduler.requestAnimationFrame);
+  const stateFrames = animationFrames.withLatestFrom(obsState,(s1,s2)=> s2);
+
+  const realizerStream = stateFrames.scan( function(realizer, atom){
+    const newTree = renderMarbles(atom.toJSON());
+    const nextRealizer = realizer(newTree);
+    return nextRealizer;
+  }, initialRealizer);
+
+  // Need something to pull values through. 
+  // TODO: How am I supposed to do this the Rx Way?
+  realizerStream.subscribe( ()=>{} )
+
+  return recordObservation;
+}
--- js/visualization.js
+++ js/visualization.js
@@ -2,60 +2,14 @@ import Rx from 'rx';
 import Immutable from 'immutable';
 import createInitialRealizer from './lib/virtualDomRealizer';
 import renderMarbles from './lib/visual';
+import createMarbleDisplay from './lib/marbles';
 
-const initialRealizer = createInitialRealizer(document.getElementsByTagName('main')[0]);
-
-let observations = new Rx.Subject(); 
-
-function recordObservation(streamId,observation){
-  observations.onNext({streamId,observation});
-}
-
-const animationFrames = Rx.Observable.interval(50,Rx.Scheduler.requestAnimationFrame);
-
-const obsState = observations.scan( function(observables,{streamId,observation}){
-  const EMPTY_OBSERVABLE = Immutable.Map({streamName:streamId,observations:Immutable.List()})
-
-  return observables.update( streamId, EMPTY_OBSERVABLE, function(observable){
-    return observable.update('observations', (obs)=> obs.push(observation));
-  });
-},Immutable.Map());
-
-const stateFrames = animationFrames.withLatestFrom(obsState,(s1,s2)=> s2);
-
-const realizerStream = stateFrames.scan( function(realizer, atom){
-  const newTree = renderMarbles(atom.toJSON());
-  const nextRealizer = realizer(newTree);
-  return nextRealizer;
-}, initialRealizer);
-
-// Need something to pull values through. 
-// TODO: How am I supposed to do this the Rx Way?
-realizerStream.subscribe( ()=>{} )
-
+const recordObservation = createMarbleDisplay(document.getElementById('marbles-container'));
 
 const tickStream = Rx.Observable.interval(333);
 
 const clickStream = Rx.Observable.fromEvent(document.getElementById('click-me'),'click')
   .map(()=>'tap!');
-
-const stream = tickStream;
-
-//animationFrames.subscribe(function(s){
-  //console.log('ANIMATION',s);
-//});
-
-//stream.subscribe(function(s){
-  //console.log('STREAM',s);
-//});
-
-//observations.subscribe(function(o){
-  //console.log('OBSERVATION',o);
-//});
-
-//obsState.subscribe(function(o){
-  //console.log('OBS',o.toJSON());
-//});
 
 clickStream.timestamp().subscribe( function(s){
   recordObservation('clicks',s);
