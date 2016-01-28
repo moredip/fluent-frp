102655ecbd51a1bd5c28876447a110ac7cab5ee6
Pete Hodgson <git@thepete.net>
2016-01-27 16:10:02 -0800

    starting point

--- js/slider-to-label.js
+++ js/slider-to-label.js
@@ -14,16 +14,3 @@ function valueFromEvent(e){
 const $slider = $('.slider input'),
       $label = $('.slider .label');
 
-const values = Rx.Observable.fromEvent($slider,'input',valueFromEvent);
-
-values.visualize( 'values' );
-
-const floats = values.map( v => parseFloat(v) )
-floats.visualize( 'the floats' );
-
-const percents = floats.map( v => Math.round(100*v) );
-percents.visualize('percentages');
-
-const formatted = percents.map( p => `${p}%` );
-
-formatted.subscribe( f => $label.text(f) )
--- js/summing-two-sliders.js
+++ js/summing-two-sliders.js
@@ -11,35 +11,8 @@ function valueFromEvent(e){
   return e.target.value;
 }
 
-function valueStreamFrom($slider){
-  return Rx.Observable.fromEvent($slider,'input',valueFromEvent)
-    .startWith($slider.val())
-    .map( parseFloat );
-}
-
 const $sliderA = $('.slider.a input'),
       $sliderB = $('.slider.b input'),
       $labelA = $('.slider.a .label'),
       $labelB = $('.slider.b .label'),
       $labelSummed = $('.sum .label');
-
-const valuesA = valueStreamFrom($sliderA);
-
-valuesA.visualize('values A');
-
-const valuesB = valueStreamFrom($sliderB);
-
-valuesB.visualize('values B');
-
-valuesA.subscribe( (v) => $labelA.text(v) );
-valuesB.subscribe( (v) => $labelB.text(v) );
-
-const summedValues = Rx.Observable.combineLatest(
-    valuesA,
-    valuesB,
-    (a,b) => a+b
-    );
-
-summedValues.visualize('summed');
-
-summedValues.subscribe( (v) => $labelSummed.text(v) );
