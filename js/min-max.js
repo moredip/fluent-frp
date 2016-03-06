0f1cd384579d4a6a179d1d90348312e6bf4b49fb
Pete Hodgson <git@thepete.net>
2016-03-05 19:13:44 -0800

    misc prep

--- js/min-max.js
+++ js/min-max.js
@@ -1,23 +1,18 @@
+import Rx from 'rxjs/Rx';
+import setupMarbelous from './setup-marbelous';
+
 import NumberLine from './min-max/number-line.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Rx = Marbelous.Rx;
+setupMarbelous(Rx);
 
 const numberLineContainer = document.getElementById('number-line');
-
 function renderNumberLine(props){
   ReactDOM.render(
     <NumberLine {...props} />,
     numberLineContainer
   );
-}
-
-const recordObservation = Marbelous.createMarbleDisplay(document.getElementById('marbles-container'));
-
-Rx.Observable.prototype.visualize = function(name){
-  this.inspectTime(60).subscribe( e => recordObservation(name,e) );
-  return this;
 }
 
 function valueFromEvent(e){
@@ -27,28 +22,28 @@ function valueFromEvent(e){
 const $slider = $('.slider input'),
       $label = $('.slider .label');
 
-const numbers = Rx.Observable.fromEvent($slider,'input',valueFromEvent)
+const number$ = Rx.Observable.fromEvent($slider,'input',valueFromEvent)
   .startWith($slider.val())
   .map( parseFloat )
 
-//numbers.visualize( 'numbers' );
+number$.visualize( 'numbers' );
 
-numbers.subscribe( f => $label.text(f) );
+number$.subscribe( f => $label.text(f) );
 
 
-const mins = numbers
-  .scan( Math.min )
-  .distinctUntilChanged();
 
-const maxes = numbers
-  .scan( Math.max )
-  .distinctUntilChanged();
 
-//mins.visualize( 'mins' );
-//maxes.visualize( 'maxes' );
 
-const minsAndMaxes = Rx.Observable.combineLatest( numbers, mins, maxes, (number,min,max)=> { return {number,min,max} } );
 
-//minsAndMaxes.map( ({min,max})=> [min,max] ).visualize('number line state');
 
-minsAndMaxes.subscribe( renderNumberLine );
+
+
+
+
+
+
+
+//const viewState = Rx.Observable.combineLatest( numbers, min$, max$, (number,min,max)=> { return {number,min,max} } );
+//viewState.visualize('view state');
+//viewState.subscribe( (vs)=> console.log("view state", vs) );
+//viewState.subscribe( renderNumberLine );
--- js/setup-marbelous.js
+++ js/setup-marbelous.js
@@ -2,7 +2,7 @@ export default function setupMarbelous(Rx){
   const recordObservation = Marbelous.createMarbleDisplay(document.getElementById('marbles-container'));
 
   Rx.Observable.prototype.visualize = function(name){
-    this.subscribe( e => recordObservation(name,e) );
+    this.inspectTime(60).subscribe( e => recordObservation(name,e) );
     return this;
   }
 }
