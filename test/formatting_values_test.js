f30a77997f7a8b4b575b26d38dcdb2bec2c6685a
Pete Hodgson <git@thepete.net>
2016-03-06 13:52:29 -0800

    working test for formatted values

--- test/formatting_values_test.js
+++ test/formatting_values_test.js
@@ -1,8 +1,19 @@
 import {expect} from 'chai';
+import Rx from 'rxjs/Rx';
+
+function formatValues(values$){
+  return values$.map( v => parseFloat(v) )
+    .map( v => Math.round(100*v) )
+    .map( p => `${p}%` );
+}
 
 describe( 'formatting input values', function(){
-  it('works', function(){
-    expect(2+2).to.equal(4);
+  it('formats values correctly', function(){
+    var values$ = Rx.Observable.of("0.12","0.555","0","1.173");
+    var expectedResult = ["12%","56%","0%","117%"];
     
+    return formatValues(values$).toArray().do( function(actualResult){
+      expect(actualResult).to.eql(expectedResult);
+    }).toPromise();
   });
 });
